b3ca822b2ee2764385bdc9327d3ca29883b77c3f
Tom Wilson <twilson63@gmail.com>
2019-04-21 16:20:34 +0000

    refactored build.js

--- build.js
+++ build.js
@@ -1,70 +1,19 @@
-require('svelte/register')
 const pkg = require('./package.json')
-const fs = require('fs')
-const { map, pipe, filter, assoc, pathOr } = require('ramda')
-const yamlFront = require('yaml-front-matter')
-const marked = require('marked')
-const shell = require('shelljs')
+const { pipe, pathOr } = require('ramda')
+const dist = pathOr('dist', ['meta', 'target'], pkg)
 
-const Blog = require('./components/Blog.svelte').default
-const Post = require('./components/Post.svelte').default
+// **** require pipe functions ****
+const initBuild = require('./lib/init')(dist)
+const getContent = require('./lib/content')
+const createBlog = require('./lib/create-blog-page')(pkg)(dist)
+const createArticles = require('./lib/create-articles')(pkg)(dist)
+const copyAssets = require('./lib/assets')(dist)
 
-// pure functions
-const isMarkdown = name => /.md$/.test(name)
-const parse = name => {
-  const md = fs.readFileSync('./content/' + name, 'utf-8')
-  return yamlFront.loadFront(md)
-}
-const template = (head, html) => `<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
-    <title>${pathOr('Blog', ['meta', 'title'], pkg)}</title>
-    ${head}
-  </head>
-  <body>
-    ${html}
-  </body>
-</html>`
-
-const createArticle = article => {
- article = assoc('content', marked(article.__content), article)
-  
- const { html, head } = Post.render({ meta: pkg.meta, article })
- fs.writeFileSync('./dist/' + article.ID + '.html', template(head, html))
-}
-
-// create dist
-shell.mkdir('dist')
-// build index.html
-
-
-const files = fs.readdirSync('./content')
-const data = pipe(
-  filter(isMarkdown),
-  map(parse)
-)(files)
-
-
-const { html, head }  = Blog.render({ meta: pkg.meta, articles: data })
-// render index template
-fs.writeFileSync('./dist/index.html', template(head, html))
-
-console.log('create index.html')
-
-// build articles
-map(createArticle, data)
-
-console.log('created articles')
-
-
-// copy assets over ot dist
-//
-shell.mkdir('-p', 'dist/assets')
-shell.cp('-rf', './assets','./dist')
-console.log('---- copied assets ----')
-
-//
-
+// pipeline
+pipe(
+  initBuild,
+  getContent,
+  createBlog,
+  createArticles,
+  copyAssets
+)()
